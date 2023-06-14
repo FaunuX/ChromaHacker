2bb8ec095ea4c725c9e21ff7982cf9d7c555f9e5
FaunuX <judahpspiegel@gmail.com>
2023-06-14 14:19:10 -0700

    add support for other file extensions

--- chromahacker/palettize.py
+++ chromahacker/palettize.py
@@ -6,7 +6,7 @@ import numpy as np
 from chromahacker.spline import spline_from # hey this line is a palindrome
 from chromahacker.process_image import url_to_image
 
-def palettize(url, *args):
+def palettize(url, output, *args):
     np_array = url_to_image(url)
 
     img = cv2.cvtColor(np_array, cv2.COLOR_RGB2GRAY)
@@ -14,4 +14,4 @@ def palettize(url, *args):
     fn = spline_from(*args)
 
     display = np.rint(fn(img)).astype(np.uint8)
-    cv2.imwrite('wallpaper.jpg', display)
+    cv2.imwrite('wallpaper.' + output, display)
